6626132acc5d25dd81233fe3360a9752c8bf5cc0
Jason Edstrom <jason@instacode.io>
2022-12-23 16:01:29 -0600

    Add Data Model for Dice

---
+++ RollForInit/RollForInit/Model/RFIDiceModel.swift
@@ -0,0 +1,15 @@
+//
+//  RFIDiceModel.swift
+//  RollForInit
+//
+//  Created by Edstrom, Jason on 12/23/22.
+//  Copyright © 2022 InstaCode. All rights reserved.
+//
+
+import Foundation
+
+struct RFIDiceModel{
+	var id: Int
+	var title: String
+	var value: Int
+}
