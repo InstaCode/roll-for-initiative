2900a79217a491a0ed29a3df66d734b9fac4d4f8
Jason Edstrom <jason@instacode.io>
2022-12-23 14:13:30 -0600

    Initial Commit

---
+++ RollForInit/RollForInit/RollForInitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollForInitApp.swift
+//  RollForInit
+//
+//  Created by Edstrom, Jason-CW on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct RollForInitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
